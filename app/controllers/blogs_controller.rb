399913405a46cd84d44886a6e730898586122e26
luke <lukekennedy212@gmail.com>
2020-05-02 00:11:25 +0800

    initial design for portfolio page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5).without_count
+    @blogs = Blog.order(id: :desc).page(params[:page]).per(5).without_count
     @page_title = 'My Portfolio Blog'
   end
 
